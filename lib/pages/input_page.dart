8504cc7322aca15c64340c7d190496fe16a400ca
Jay Kumar <jaykumar20march@gmail.com>
2022-07-09 15:19:20 +0530

    File Structure Updated

---
+++ lib/pages/input_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+
+  static String id = 'InputPage';
+
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
